bd11391c4f6c8ee7269cf657a0f0f43d90982c30
a5632645 <3022602213@qq.com>
2024-10-18 12:01:52 +0800

    update

--- agui/agui/ag_ref_obj.c
+++ agui/agui/ag_ref_obj.c
@@ -1,66 +1,47 @@
 #include "agui/ag_ref_obj.h"
-#include "ag_impl/lock_impl.h"
+#include "ag_impl/mem_impl.h"
+#include "ag_impl/atomic_impl.h"
 
-// ---------------------------------------- AgRefObj ----------------------------------------
+// ---------------------------------------- private ----------------------------------------
+
+typedef struct {
+    ag_atomic_bool_t master_dead;
+    ag_atomic_int_t ref_count;
+} RefCounter;
+
+static void _DecreseCount(RefCounter* rc) {
+    AG_ATOMIC_INT_SUB(&rc->ref_count, 1);
+    if (AG_ATOMIC_INT_LOAD(&rc->ref_count) == 0) {
+        AgImpl_MemFree(rc);
+    }
+}
+
+// ---------------------------------------- public ----------------------------------------
+
+void AgRefObj_Init(AgRefObj* obj) {
+    obj->ptr = AgImpl_MemAlloc(sizeof(RefCounter));
+    ((RefCounter*)obj->ptr)->ref_count = 1;
+    ((RefCounter*)obj->ptr)->master_dead = ag_false;
+}
+
+void AgRefObj_DestoryMaster(AgRefObj* obj) {
+    RefCounter* rc = (RefCounter*)obj->ptr;
+    AG_ATOMIC_INT_STORE(&rc->master_dead, ag_true);
+    _DecreseCount(rc);
+}
+
 ag_bool AgRefObj_IsMasterDead(AgRefObj* obj) {
-    if (NULL == obj) {
-        return ag_true;
-    }
-    return AG_ATOMIC_BOOL_LOAD(&obj->master_dead);
+    RefCounter* rc = (RefCounter*)obj->ptr;
+    return AG_ATOMIC_BOOL_LOAD(&rc->master_dead);
 }
 
-// ---------------------------------------- AgMasterRefObj ----------------------------------------
-void AgMasterRefObj_Init(AgMasterRefObj* obj) {
-    if (NULL == obj) {
-        return;
-    }
-    AgList_Init(&obj->refs);
-    obj->dead = ag_false;
-    obj->lock = AgImpl_LockCreate();
+void AgRefObj_AddRef(AgRefObj* obj, AgRefObj* master) {
+    obj->ptr = master->ptr;
+    RefCounter* rc = (RefCounter*)obj->ptr;
+    AG_ATOMIC_INT_ADD(&rc->ref_count, 1);
 }
 
-void AgMasterRefObj_AddRef(AgMasterRefObj* obj, AgRefObj* ref) {
-    if (NULL == obj || NULL == ref) {
-        return;
-    }
-    AgImpl_Lock(obj->lock);
-    ref->master_dead = obj->dead;
-    if (ag_false == obj->dead) {
-        AgList_PushBack(&obj->refs, &ref->node);
-    }
-    AgImpl_Unlock(obj->lock);
-}
-
-void AgMasterRefObj_Remove(AgMasterRefObj* obj, AgRefObj* ref) {
-    if (NULL == obj || NULL == ref) {
-        return;
-    }
-    AgImpl_Lock(obj->lock);
-    if (ag_false == AgRefObj_IsMasterDead(ref) && ag_false == obj->dead) {
-        AgList_Remove(&obj->refs, &ref->node);
-    }
-    AgImpl_Unlock(obj->lock);
-}
-
-void AgMasterRefObj_Destroy(AgMasterRefObj* obj) {
-    if (NULL == obj) {
-        return;
-    }
-
-    AgImpl_Lock(obj->lock);
-    obj->dead = ag_true;
-    AgListNode* node = obj->refs.head;
-    while (NULL != node) {
-        AgListNode* next = node->next;
-        AgRefObj* ref = AGUI_CONTAINER_OF(AgRefObj, node, node);
-        AG_ATOMIC_BOOL_STORE(&ref->master_dead, ag_true);
-        node->next = NULL;
-        node->prev = NULL;
-        node = next;
-    }
-    obj->refs.head = NULL;
-    obj->refs.tail = NULL;
-    AgImpl_Unlock(obj->lock);
-
-    AgImpl_LockDestroy(obj->lock);
+void AgRefObj_DestorySlave(AgRefObj* obj) {
+    RefCounter* rc = (RefCounter*)obj->ptr;
+    _DecreseCount(rc);
 }
--- agui/include/ag_impl/atomic_impl.h
+++ agui/include/ag_impl/atomic_impl.h
@@ -10,3 +10,9 @@
 typedef atomic_bool ag_atomic_bool_t;
 #define AG_ATOMIC_BOOL_STORE(p, v) atomic_store(p, v)
 #define AG_ATOMIC_BOOL_LOAD(p) atomic_load(p)
+
+typedef atomic_int ag_atomic_int_t;
+#define AG_ATOMIC_INT_STORE(p, v) atomic_store(p, v)
+#define AG_ATOMIC_INT_LOAD(p) atomic_load(p)
+#define AG_ATOMIC_INT_ADD(p, v) atomic_fetch_add(p, v)
+#define AG_ATOMIC_INT_SUB(p, v) atomic_fetch_sub(p, v)
